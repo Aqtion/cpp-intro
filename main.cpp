54b8453960f37a147dec0830b3f85b35817c7b3b
Aqtion <keshavbadrinath06@gmail.com>
2023-07-18 11:15:58 -0400

    using namespace

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 int add(int x, int y);
 int subtract(int x, int y);
@@ -23,16 +24,16 @@ int divide(int x, int y) {
 
 int main()
 {
-  std::cout << "Keshav Badrinath, Building 24 Room 115" << std::endl;
+  cout << "Keshav Badrinath, Building 24 Room 115" << "\n";
 
   int x = 5;
   int y = 7;
   int z = 8;
   int w = 9;  
   int v = 35;
-  std::cout << "The sum of " << x << " and " << y << " and " << z << " is " << x+y+z << "\n";
-  std::cout << "The product of " << x << " and " << y << " and " << z << " and " << w << " is " << w*x*y*z << "\n";
-  std::cout << "The division of " << x << " and " << y << " and " << v << " is " << v / x / y << "\n";
+  cout << "The sum of " << x << " and " << y << " and " << z << " is " << x+y+z << "\n";
+  cout << "The product of " << x << " and " << y << " and " << z << " and " << w << " is " << w*x*y*z << "\n";
+  cout << "The division of " << x << " and " << y << " and " << v << " is " << v / x / y << "\n";
 
   // std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
 
